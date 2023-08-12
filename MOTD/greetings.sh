ad720974f7ce66e85489e81d31bc10d65ed23d24
Beeranco <49772805+Beeranco@users.noreply.github.com>
2023-08-12 13:36:29 +0200

    Update greetings.sh
    Fixed oopsie

--- MOTD/greetings.sh
+++ MOTD/greetings.sh
@@ -2,5 +2,5 @@ GREEN='\033[1;32m'
 WHITE='\033[1;37m'
 
 echo ""
-echo -e "Welcome ${GREEN}Jarno${WHITE}! To run the updater enter the following to the console:"
+echo -e "Welcome ${GREEN}%name%${WHITE}! To run the updater enter the following to the console:"
 echo "bash /opt/updater.sh"
