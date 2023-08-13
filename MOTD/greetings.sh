e54fdbd64ea76a19f1bc9c7fc0cd9b0d81552fbd
Beeranco <49772805+Beeranco@users.noreply.github.com>
2023-08-13 16:50:23 +0200

    Update greetings.sh
    Fixed color reset back to white

--- MOTD/greetings.sh
+++ MOTD/greetings.sh
@@ -1,6 +1,6 @@
 GREEN='\033[1;32m'
-WHITE='\033[1;37m'
+RESET='\033[0m'
 
 echo ""
-echo -e "Welcome ${GREEN}%name%${WHITE}! To run the updater enter the following to the console:"
+echo -e "Welcome ${GREEN}%name%${RESET}! To run the updater enter the following to the console:"
 echo "bash /opt/updater.sh"
